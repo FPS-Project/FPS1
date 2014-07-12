e3747319b06c735818ad2052b2db1e0001e2ffd8
Cons3ssiO <KINGJANDA@HOTMAIL.COM>
2014-07-12 17:48:10 +0200

    init

---
+++ New Unity Project 1/Assets/MeleeSystem.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var TheDammage : int = 50;
+var Distance : float;
+var MaxDistance : float = 1.5;
+var TheStick : Transform;
+
+function Update ()
+{
+	if (Input.GetButtonDown ("Fire1"))
+	{
+		TheStick.animation.Play("MeleeAttack");
+		var hit : RaycastHit;
+		if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit))
+		{
+			Distance = hit.distance;
+			if (Distance < MaxDistance)
+			{
+			hit.transform.SendMessage("ApplyDammage", TheDammage, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
