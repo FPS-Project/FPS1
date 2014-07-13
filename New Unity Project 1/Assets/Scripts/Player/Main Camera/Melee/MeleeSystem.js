12b904d4c2bc312aca64054c0314194342c72c1d
Cons3ssiO <kingjanda@hotmail.com>
2014-07-13 15:56:16 +0200

    Upload_2

--- New Unity Project 1/Assets/Scripts/Player/Main Camera/Melee/MeleeSystem.js
+++ New Unity Project 1/Assets/Scripts/Player/Main Camera/Melee/MeleeSystem.js
@@ -9,7 +9,7 @@ function Update ()
 {
 	if (Input.GetButtonDown ("Fire1"))
 	{
-		TheStick.animation.Play("Attack");
+		TheStick.animation.Play("StickAttack");
 		var hit : RaycastHit;
 		if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit))
 		{
@@ -20,8 +20,19 @@ function Update ()
 			}
 		}
 	}
+	
 	if (TheStick.animation.isPlaying == false)
 	{
-		TheStick.animation.CrossFade("IdleStick");
+		TheStick.animation.CrossFade("StickIdle");
+	}
+	
+	if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.D))
+	{
+		TheStick.animation.CrossFade ("StickWalk");
+	}
+	
+	if (Input.GetKey (KeyCode.LeftShift))
+	{
+		TheStick.animation.CrossFade ("StickRun");
 	}
 }
---
+++ New Unity Project 1/Assets/Scripts/Player/Main Camera/Walk.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var TheWalk : Transform;
+
+function Update ()
+{
+	if (TheWalk.animation.isPlaying == false)
+	{
+		TheWalk.animation.CrossFade("PlayerIdle");
+	}
+	
+	if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.D))
+	{
+		TheWalk.animation.CrossFade ("PlayerWalk");
+	}
+	
+	if (Input.GetKey (KeyCode.LeftShift))
+	{
+		TheWalk.animation.CrossFade ("PlayerRun");
+	}
+	
+	if (Input.GetKeyDown (KeyCode.LeftShift))
+	{
+		TheWalk.animation.Stop ("PlayerWalk");
+	}
+	
+	if (Input.GetKeyUp (KeyCode.LeftShift))
+	{
+		TheWalk.animation.CrossFade ("PlayerWalk");
+	}
+}
