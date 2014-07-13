d5411448561d4adaed00b9c83d2df097688595a4
Cons3ssiO <KINGJANDA@HOTMAIL.COM>
2014-07-13 13:04:52 +0200

    upload_1

--- New Unity Project 1/Assets/MeleeSystem.js
+++ New Unity Project 1/Assets/MeleeSystem.js
@@ -9,7 +9,7 @@ function Update ()
 {
 	if (Input.GetButtonDown ("Fire1"))
 	{
-		TheStick.animation.Play("MeleeAttack");
+		TheStick.animation.Play("Attack");
 		var hit : RaycastHit;
 		if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit))
 		{
@@ -19,5 +19,9 @@ function Update ()
 			hit.transform.SendMessage("ApplyDammage", TheDammage, SendMessageOptions.DontRequireReceiver);
 			}
 		}
+	}
+	if (TheStick.animation.isPlaying == false)
+	{
+		TheStick.animation.CrossFade("IdleStick");
 	}
 }
---
+++ New Unity Project 1/Assets/Walk.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var TheWalk : Transform;
+
+function Update ()
+{
+	if (Input.GetButtonDown ("Vertical"))
+	{
+		TheWalk.animation.Play("walk");
+	}
+	else if (Input.GetButtonUp("Vertical"))
+	{
+    	TheWalk.animation.Stop("walk");
+    }
+}
